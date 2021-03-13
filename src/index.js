ab9fca16cc812443f3a65813865eceef8b335a9f
yuka-sakamaki <69895997+super-sausage@users.noreply.github.com>
2021-03-13 17:42:00 +0900

    Squareコンポーネント更新

--- src/index.js
+++ src/index.js
@@ -3,18 +3,32 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: null,
+    };
+  }
   render() {
     return (
-      <button className="square">
-        {/* TODO */}
+      <button 
+      className="square"
+      onClick={() => this.setState({value: 'X'})}
+      >
+        {this.state.value}
       </button>
     );
   }
 }
+// setState をコンポーネント内で呼び出すと、React はその内部の子コンポーネントも自動的に更新します。
+// constructor を持つ React のクラスコンポーネントでは、すべてコンストラクタを super(props) の呼び出しから始めるべきです。
+// Square（マス目）コンポーネントは 1 つの <button> をレンダーし、Board（盤面）が 9 個のマス目をレンダーしています。
+// React では、親から子へと props を渡すことで、アプリ内を情報が流れていきます。
+
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i} />;
   }
 
   render() {
